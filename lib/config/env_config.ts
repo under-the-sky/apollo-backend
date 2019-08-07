8101065df7ee0b65d72b73e0e35ae0ed2c0f75e7
taochen <taochen@ebay.com>
2019-08-07 12:13:57 +0800

    change swagger option host for prod

--- lib/config/env_config.ts
+++ lib/config/env_config.ts
@@ -1,5 +1,6 @@
 import dotenv = require('dotenv');
 import fs = require('fs');
+import { options } from './swaggerconfig';
 
 if (fs.existsSync(".env")) {
     console.debug("Using .env file to supply config environment variables");
@@ -13,7 +14,7 @@ const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
 export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
-
+export const OPTIONS = prod ? options.swaggerDefinition.host = 'api.tynb0622.com' : options;
 if (!SESSION_SECRET) {
     console.error("No client secret. Set SESSION_SECRET environment variable.");
     process.exit(1);
